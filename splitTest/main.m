215373103472abd1a428378bdd275a7bcbdd64b0
BENJAMIN JOHNSON <benjaminjohnson@BENJAMINs-MacBook-Air.local>
2016-12-01 10:42:44 -0600

    Initial Commit

---
+++ splitTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  splitTest
+//
+//  Created by BENJAMIN JOHNSON on 12/1/16.
+//  Copyright © 2016 BENJAMIN JOHNSON. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
